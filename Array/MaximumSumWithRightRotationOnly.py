6d1c2685529d56fb876d5557d89238a6bdd250ce
krishna5737 <krishan57376@gmail.com>
2019-06-06 14:48:48 +0530

    Update and rename MaximumSumWithRotationOnly.py to MaximumSumWithRightRotationOnly.py

--- Array/MaximumSumWithRightRotationOnly.py
+++ Array/MaximumSumWithRightRotationOnly.py
@@ -1,5 +1,5 @@
 #Problem Statement:
-#Find maximu#m value of Sum( i*arr[i]) with only rotations on given array allowed
+#Find maximu#m value of Sum( i*arr[i]) with only right rotations on given array allowed
 # R0 = 0*a[0] + 1*a[1] + 2*a[2] + 3*a[3] + .......................... + (n-2)*a[n-2] + (n-1)*a[n-1]
 # R1 = 1*a[0] + 2*a[1] + 3*a[2] + 4*a[3] + .......................... + (n-1)*a[n-2] + 0*a[n-1]
 # R2 = 2*a[0] + 3*a[1] + 4*a[2] + 5*a[3] + .......................... + (0)*a[n-2] + 1*a[n-1]
