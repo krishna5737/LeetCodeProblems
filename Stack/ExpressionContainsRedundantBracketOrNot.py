0a7f0a4d5fb595ed1682b3a540fd2749ba044bbf
krishna5737 <krishan57376@gmail.com>
2019-07-09 16:44:08 +0530

    Create ExpressionContainsRedundantBracketOrNot.py

---
+++ Stack/ExpressionContainsRedundantBracketOrNot.py
@@ -0,0 +1,38 @@
+#Given a string of balanced expression, find if it contains a redundant parenthesis or not. 
+#A set of parenthesis are redundant if same sub-expression is surrounded by unnecessary or multiple brackets. 
+#Print ‘Yes’ if redundant else ‘No’.
+# Input: 
+# ((a+b))
+# (a+(b)/c)
+# (a+b*(c-d))
+# Output: 
+# Yes
+# Yes
+# No
+
+def checkRedundancy(str):
+	stack = []
+	for i in str:
+		if(i==')'):
+			top = stack.pop()
+			flag = True
+			while(top!='('
+				if(top == '+' or top == '-' or top == '*' or top == '/' ):
+					flag = False
+				top = stack.pop()
+			if(flag == True):
+				return True
+		else:
+			stack.append(i)
+	
+array = ["((a+b))", "(a+(b)/c)"]
+for j in array:
+	
+	if(not(checkRedundancy(j))):
+		print("No Redundancy")
+	else:
+		print("Redundancy")
+	
+
+
+		
