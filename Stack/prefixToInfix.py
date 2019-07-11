a676ccf634e40e64e8e08dc8507070b0abcd7cb5
krishna5737 <krishan57376@gmail.com>
2019-07-11 20:21:13 +0530

    Create prefixToInfix.py

---
+++ Stack/prefixToInfix.py
@@ -0,0 +1,20 @@
+# Read the Prefix expression in reverse order (from right to left)
+# If the symbol is an operand, then push it onto the Stack
+# If the symbol is an operator, then pop two operands from the Stack
+# Create a string by concatenating the two operands and the operator between them.
+# string = (operand1 + operator + operand2)
+# And push the resultant string back to Stack
+# Repeat the above steps until end of Prefix expression.
+
+def PrefixToPostfix(str):
+    stack = []
+    for j in range(len(str)-1,-1,-1):
+        i = str[j]
+        if(i.isalpha()):
+            stack.append(i)
+        else:
+            firstcharacter = stack.pop()
+            secondcharacter = stack.pop()
+            stack.append("("+firstcharacter+i+secondcharacter+")")
+    return(stack[0])
+print(PrefixToPostfix("*-A/BC-/AKL"))
